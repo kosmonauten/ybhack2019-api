58643cddc18074630e3ac03002cb0fbe5f4ac2a2
Rajasekaran Yogarajah <r.yogarajah@tecgroup.ch>
2019-11-15 18:51:30 +0100

    feat(cross-origin): Added Cross Origin

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.use(
     extended: true,
   })
 )
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
