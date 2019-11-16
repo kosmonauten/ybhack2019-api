25fb0d099a6a2c4accb26cd8393a0de462caa878
Rajasekaran Yogarajah <r.yogarajah@tecgroup.ch>
2019-11-16 01:17:22 +0100

    feat(popularityindex): Added Popularityindex

--- api/v2/player.js
+++ api/v2/player.js
@@ -1,23 +1,45 @@
 const pool = require('../../db')
 
+const getPopularityIndex = async () => {
+  const resPopularity = await pool.query(`
+    SELECT scoring.id, SUM(scoring.score) AS score FROM (
+      SELECT player.id, media_sentiment_article.title, AVG(media_sentiment_sent_list."value") AS score
+      FROM player, media_sentiment_player, media_sentiment_article, media_sentiment_sent_list
+      WHERE player.id = media_sentiment_player.player
+          AND media_sentiment_player.article = media_sentiment_article.id
+          AND media_sentiment_sent_list.article = media_sentiment_article.id
+      GROUP BY player.id, media_sentiment_article.title
+      ) AS scoring
+    GROUP BY scoring.id`)
+    .then(res => res)
+    .catch(err => { throw err })
+    
+    return resPopularity.rows.reduce((r, e) => {
+      r[e.id] = e.score;
+      return r;
+    }, {});
+}
+
 const getUsers = async (request, response) => {
     output = []
 
-    const resOne = await pool.query('SELECT id, firstname, lastname FROM player ORDER BY id ASC')
+    const resOne = await pool.query('SELECT id, firstname, lastname, playerimage FROM player ORDER BY id ASC')
     .then(res => res)
     .catch(err => {throw err})
 
+    const popularityIndex = await getPopularityIndex()
+
     for(entry in resOne.rows) {
       row = resOne.rows[entry]
-      console.log(row)
+      if(row.playerimage == null) { row.playerimage = 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png'}
       output.push({
         'id': row.id,
         'firstname': row.firstname,
         'lastname': row.lastname,
         'performance': "19%",
-        'popularity': "99%",
+        'popularity': (popularityIndex[row.id] * 100).toFixed(2) + '%',
         "transfermarkt": 235252,
-        "img": "https://upload.wikimedia.org/wikipedia/commons/3/3a/Marco_W%C3%B6lfli.jpg"
+        "img": row.playerimage
       })
     }
     
@@ -25,10 +47,10 @@ const getUsers = async (request, response) => {
 
 }
 
-const getUserById = (request, response) => {
+const getUserById = async (request, response) => {
     const id = request.params.id
   
-    pool.query('SELECT * FROM player WHERE id = $1', [id], (error, results) => {
+    const resOne = await pool.query('SELECT * FROM player WHERE id = $1', [id], (error, results) => {
       if (error) {
         throw error
       }
